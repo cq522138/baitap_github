a278343abbf8663fee3a60892aec944edd9c8dc7
cq522138 <tuanlinhvu1991@gmail.com>
2018-08-09 10:14:22 +0700

    bai tap java

---
+++ giai_phuong_trinh_bac_2.js
@@ -0,0 +1,43 @@
+function ketqua() {
+
+  var a = parseFloat(document.getElementById('numbera').value);
+  var b = parseFloat(document.getElementById('numberb').value);
+  var c = parseFloat(document.getElementById('numberc').value);
+  var denta = (b*b)-(4*a*c);
+
+  if (a == 0) {
+    alert("khong phai la phuong pt 2");
+  } else if
+
+
+
+
+
+
+  // var x1=(-b+Math.sqrt(z))/(2*a);
+  // var x2=(-b-math.sqrt(z))/(2*a);
+  // var x=-b/(a*2);
+  // var x3=-c/b;
+  //
+  // if(a!=0){
+  //   if(z>0){
+  //     document.writeln("phuong trinh co 2 nghiem phan biet" +x1+"va" +x2);
+  //   }
+  //   else if(z=0){
+  //     document.writeln("phuong trinh co nghiem kep");
+  //   }
+  //   else
+  //     document.writeln("phuong trinh vo nghiem");
+  // }
+  // else{
+  //   if(b!=0){
+  //     document.writeln("phuong trinh co 1 nghiem "+x3);
+  //   }
+  //   else
+  //     document.writeln("khong co nghiem cho pt nay");
+  // }
+}
+
+
+for (var i=1;i<=10;i++)
+
